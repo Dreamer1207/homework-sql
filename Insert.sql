9aab240562b701d7f3cc228189d0b766f6c1d30b
Roman Zaverskiy <Roma12071981@mail.ru>
2023-04-27 11:23:27 +0300

    Борис Гребенщиков, изменения

--- Insert.sql
+++ Insert.sql
@@ -15,7 +15,7 @@ VALUES(5, 'Шансон');
 
 
 INSERT INTO performer(id, name)
-VALUES(1, 'МакSим');
+VALUES(1, 'Полина Гагарина');
 
 INSERT INTO performer(id, name)
 VALUES(2, 'Альянс');
@@ -30,63 +30,51 @@ INSERT INTO performer(id, name)
 VALUES(5, 'Елена Ваенга');
 
 INSERT INTO performer(id, name)
-VALUES(6, 'Баста');
+VALUES(6, 'Михаил Круг');
 
 INSERT INTO performer(id, name)
-VALUES(7, 'Григорий Лепс');
+VALUES(7, 'Борис Гребенщиков');
 
 INSERT INTO performer(id, name)
 VALUES(8, 'Моральный кодекс');
 
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(1, '1', '1');
+VALUES(1, 1, 1);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(2, '1', '5');
+VALUES(2, 1, 2);
 --
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(3, '1', '6');
-
-INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(4, '1', '7');
+VALUES(3, 1, 5);
 --
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(5, '2', '2');
+VALUES(4, 2, 2);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(6, '2', '6');
+VALUES(5, 2, 7);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(7, '2', '7');
+VALUES(6, 2, 8);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(8, '2', '8');
+VALUES(7, 3, 3);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(9, '3', '3');
+VALUES(8, 4, 4);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(10, '4', '4');
+VALUES(9, 5, 5);
 
 INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(11, '4', '6');
-
-INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(12, '5', '5');
-
-INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(13, '5', '7');
-
-INSERT INTO genreperformer(id, genre_id, performer_id)
-VALUES(14, '1', '2');
+VALUES(10, 5, 6);
 
 
 INSERT INTO album(id, name, year_release)
-VALUES(1, 'Мой рай', '2007');
+VALUES(1, 'Обезоружена', '2017');
 
 INSERT INTO album(id, name, year_release)
-VALUES(2, 'На заре', '2019');
+VALUES(2, 'На Заре', '2019');
 
 INSERT INTO album(id, name, year_release)
 VALUES(3, 'Кровь за кровь', '1991');
@@ -98,49 +86,96 @@ INSERT INTO album(id, name, year_release)
 VALUES(5, 'Клавиши', '2009');
 
 INSERT INTO album(id, name, year_release)
-VALUES(6, 'Баста 3', '2010');
+VALUES(6, 'Посвящение', '2002');
 
 INSERT INTO album(id, name, year_release)
-VALUES(7, 'Наутро', '2018');
+VALUES(7, 'Время N', '2018');
 
 INSERT INTO album(id, name, year_release)
 VALUES(8, 'Славянские танцы', '2007');
 
 
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(1, '1', '1');
+VALUES(1, 1, 1);
 
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(2, '2', '2');
+VALUES(2, 2, 2);
 --
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(3, '3', '3');
+VALUES(3, 3, 3);
 
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(4, '4', '4');
+VALUES(4, 4, 4);
 --
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(5, '5', '5');
+VALUES(5, 5, 5);
 
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(6, '6', '6');
+VALUES(6, 6, 6);
 
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(7, '7', '7');
+VALUES(7, 7, 7);
 
 INSERT INTO performeralbum(id, performer_id, album_id)
-VALUES(8, '8', '8');
+VALUES(8, 8, 8);
+
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(1, 'Спектакль окончен', '05:30', 1);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(2, 'Кукушка', '04:12', 1);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(3, 'На Заре', '05:45', 2);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(4, 'Прощай норфолк!', '05:20', 3);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(5, 'Антихрист', '05:04', 3);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(6, 'Серебро', '04:01', 4);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(7, 'Весна', '04:51', 4);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(8, 'Романс', '03:48', 4);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(9, 'Не Любил', '04:09', 5);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(10, 'Клавиши', '03:45', 5);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(11, 'Магадан', '4:26', 6);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(12, 'Приходите в мой дом', '3:41', 6);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(13, 'На ржавом ветру', '3:01', 7);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(14, 'В твоих глазах', '3:54', 8);
+
+INSERT INTO track(id, name, duration, album_id)
+VALUES(15, 'Первый снег', '4:00', 8);
 
 
 
 
-SELECT * FROM performeralbum;
+SELECT * FROM track;
 SELECT * FROM PerformerAlbum;
 SELECT * FROM GenrePerformer;
 
 
---DELETE FROM genreperformer;
+DELETE FROM track
+WHERE id = ;
 
---UPDATE performer  
---SET name = 'МакSим'
---WHERE id = 1;
+UPDATE performer  
+SET name = 'Борис Гребенщиков'
+WHERE id = 7;
